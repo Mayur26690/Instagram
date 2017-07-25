6f1e89e23d4f73d3576ff128ef0887f933d38fd1
mayur26690 <mayur26690@gmail.com>
2017-07-25 15:18:42 -0700

    small changes

--- insta/urls.py
+++ insta/urls.py
@@ -15,10 +15,10 @@ urlpatterns = [
     url(r'^login/$',auth_views.login,{'template_name': 'insta/registration/login.html'}, name='login'),
     url(r'^logout/$',auth_views.logout,{'template_name': 'insta/registration/logout.html'}, name='logout'),
 
-    url(r'^password-change/$', auth_views.password_change,{'template_name': 'insta/registration/password_change_form'}, name='password_change'),
-    url(r'^password-change/done/$', auth_views.password_change_done,{'template_name': 'insta/registration/password_change_done'}, name='password_change_done'),
-    url(r'^password_reset/$', auth_views.password_reset, name='password_reset'),
-    url(r'^password_reset/done/$', auth_views.password_reset_done, name='password_reset_done'),
+    url(r'^password-change/$', auth_views.password_change, name='password_change'),
+    url(r'^password-change/done/$', auth_views.password_change_done, name='password_change_done'),
+    url(r'^password-reset/$', auth_views.password_reset, name='password_reset'),
+    url(r'^password-reset/done/$', auth_views.password_reset_done, name='password_reset_done'),
     url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',auth_views.password_reset_confirm, name='password_reset_confirm'),
     url(r'^reset/done/$', auth_views.password_reset_complete, name='password_reset_complete'),
 
